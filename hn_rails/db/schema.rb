a9271a44cd6ab6d82f50ec6a676f5ef204119c1e
Amal Hussein <amalhussein08@gmail.com>
2013-11-07 18:10:24 -0500

    setup my posts controller

--- hn_rails/db/schema.rb
+++ hn_rails/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107215103) do
+ActiveRecord::Schema.define(version: 20131107223234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "link"
+    t.text     "body"
+    t.integer  "up_votes"
+    t.integer  "down_votes"
+    t.integer  "total_votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
