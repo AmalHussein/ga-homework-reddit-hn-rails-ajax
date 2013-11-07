79da8c02061b9cc507d9a23e3b40530b84785848
Amal Hussein <amalhussein08@gmail.com>
2013-11-07 16:43:53 -0500

    initial setup of rails app

---
+++ hn_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HnRails::Application.config.secret_key_base = 'da4b81232d10e9ee875c5f270325c04bbbc764e7a4b07a8c8b4ffa4bf9aac2bec717856d2023dc94e0b8217ff1aa64b0a9b84c175f77d869a9f94d573be1ea08'
